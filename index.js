0529b9a6cc9798364c7718617995ea10f3de3ff6
Aleksi Pöyhtäri <Aleksi.poyhtari@gmail.com>
2014-09-10 21:47:17 +0300

    Added indendations to make code readable. I admit that is was a unreadable glarp before this commit.

--- index.js
+++ index.js
@@ -10,72 +10,71 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(file);
 var path = require('path');
 var selectedDb = "logmessages";
-var child = new (forever.Monitor)('index.js', {
-  max: 3,
-  silent: true,
-  options: []
+var child = new(forever.Monitor)('index.js', {
+    max: 3,
+    silent: true,
+    options: []
 });
-	child.on('exit', function() {
-		console.log('crashed and exited after 3 restarts');
+child.on('exit', function() {
+    console.log('crashed and exited after 3 restarts');
 });
 
 child.start();
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
 });
 
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/css/pure", express.static(__dirname + '/css/pure'));
 
-db.serialize(function(){
-	if(!exists) {
-		db.run("CREATE TABLE " + selectedDb +"(info TEXT)");
-		console.log('no database exists, creating a new one');
-	}
+db.serialize(function() {
+    if (!exists) {
+        db.run("CREATE TABLE " + selectedDb + "(info TEXT)");
+        console.log('no database exists, creating a new one');
+    }
 
 });
-io.on('connection', function(socket){
-console.log('user connected, inputting older messages');
-db.each('SELECT rowid AS id, info FROM ' + selectedDb , function(err, row){
-		io.emit('chat message', row.info);
-});
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-var stmt = db.prepare('INSERT INTO '+ selectedDb +' VALUES(?)');
-stmt.run(msg);
-stmt.finalize();
-console.log('entry added to database');
-	
-  });
-  socket.on('select', function(select){
-		selectedDb = select;
-		console.log(selectedDb);
-		if(!exists)
-			db.run("CREATE TABLE " + selectedDb +"(info TEXT)");
-			console.log('no database by this name exists, creating a new one');
-	
-	db.each('SELECT rowid AS id, info FROM ' + selectedDb , function(err, row){
-		io.emit('chat message', row.info);
-		console.log('sending out old logs from'+selectedDb);
+io.on('connection', function(socket) {
+    console.log('user connected, inputting older messages');
+    db.each('SELECT rowid AS id, info FROM ' + selectedDb, function(err, row) {
+        io.emit('chat message', row.info);
+    });
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+        var stmt = db.prepare('INSERT INTO ' + selectedDb + ' VALUES(?)');
+        stmt.run(msg);
+        stmt.finalize();
+        console.log('entry added to database');
 
-});
-		console.log('database: ' + select + ' selected');
-	});
-  socket.on('make', function(newDB){
-		console.log('got a message to create some life'+newDB);
-		if(!exists)
-		db.run("CREATE TABLE " + newDB +"(info TEXT)");
-		console.log('doned it');
+    });
+    socket.on('select', function(select) {
+        selectedDb = select;
+        console.log(selectedDb);
+        if (!exists)
+            db.run("CREATE TABLE " + selectedDb + "(info TEXT)");
+        console.log('no database by this name exists, creating a new one');
 
-});
-		console.log('database: ' + selectedDb + ' selected');
-	
-});
+        db.each('SELECT rowid AS id, info FROM ' + selectedDb, function(err, row) {
+            io.emit('chat message', row.info);
+            console.log('sending out old logs from' + selectedDb);
 
+        });
+        console.log('database: ' + select + ' selected');
+    });
+    socket.on('make', function(newDB) {
+        console.log('got a message to create some life' + newDB);
+        if (!exists)
+            db.run("CREATE TABLE " + newDB + "(info TEXT)");
+        console.log('doned it');
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+    });
+    console.log('database: ' + selectedDb + ' selected');
 
 });
 
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+
+});

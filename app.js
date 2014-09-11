a85ea40851a2aca782c29dd1e37d49aabb742de8
Pete Sevander <pete.sevander@gmail.com>
2014-09-11 12:14:37 +0300

    Working, with one channel at the moment. Still a work in progress

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(dbFile);
 
 var path = require('path');
+var bodyParser = require('body-parser')
 
 var child = new(forever.Monitor)('app.js', {
     max: 3,
@@ -27,21 +28,38 @@ app.set('view engine', 'jade')
 app.set('views', './views')
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/css/pure", express.static(__dirname + '/css/pure'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res) {
-    db.all('SELECT name FROM Channels', function(err, rows) {
-        var channels = rows.map(function(row) {return row.name});
-        //console.log(channels);
-        res.render('index', {channels: channels});
-        
+    db.all('SELECT id, name FROM Channels', function(err, chans) {
+        db.all('SELECT text FROM Messages WHERE channel=0', function(err, msgs) {
+
+            res.render('index', {channels: chans, messages: msgs});
+        });
     });
 });
-app.get('/channel/', function(req, res) {
+app.post('/channel/', function(req, res) {
+    var stmt = db.prepare("INSERT INTO Channels(name) VALUES(?)");
+    stmt.run(req.body.newDBName);
 
+    console.log("new channel: " + req.body.newDBName);
+
+    res.redirect("/");
+});
+
+app.post('/channel/:id', function(req, res) {
+
+    var stmt = db.prepare("INSERT INTO Messages(text, channel) VALUES(?, ?)");
+    stmt.run(req.body.message, req.params.id);
+
+    console.log(req.body.message, req.params.id);
+
+    res.redirect("/")
 });
 
 db.serialize(function() {
     if (!exists) {
+        console.log('no database exists, creating a new one');
         // first run
         db.run("CREATE TABLE Channels (id INTEGER PRIMARY KEY AUTOINCREMENT, \
                                        name TEXT NOT NULL UNIQUE)");
@@ -52,42 +70,40 @@ db.serialize(function() {
                                        channel INTEGER NOT NULL DEFAULT 0, \
                                        FOREIGN KEY (channel) REFERENCES Channels(id) \
                                            ON DELETE CASCADE)");
-
-        console.log('no database exists, creating a new one');
     }
 });
 
 
-io.on('connection', function(socket) {
-    console.log('user connected, inputting older messages');
-    db.each('SELECT id, text FROM Messages', function(err, row) {
-        io.emit('chat message', row.text);
-    });
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
-        var stmt = db.prepare('INSERT INTO Messages(text) VALUES(?)');
-        stmt.run(msg);
-        stmt.finalize();
-        console.log('entry added to database');
+// io.on('connection', function(socket) {
+//     console.log('user connected, inputting older messages');
+//     db.each('SELECT id, text FROM Messages', function(err, row) {
+//         io.emit('chat message', row.text);
+//     });
+//     socket.on('chat message', function(msg) {
+//         io.emit('chat message', msg);
+//         var stmt = db.prepare('INSERT INTO Messages(text) VALUES(?)');
+//         stmt.run(msg);
+//         stmt.finalize();
+//         console.log('entry added to database');
 
-    });
-    socket.on('select', function(select) {
-        console.log(select);
+//     });
+//     socket.on('select', function(select) {
+//         console.log(select);
 
-        db.each('SELECT id, text FROM Messages', function(err, row) {
-            io.emit('chat message', row.info);
-            console.log('sending out old logs from' + select);
-        });
-        console.log('database: ' + select + ' selected');
-    });
-    socket.on('make', function(newDB) {
-        console.log('got a message to create some life' + newDB);
-        if (!exists)
-            db.run("CREATE TABLE " + newDB + "(info TEXT)");
-        console.log('doned it');
+//         db.each('SELECT id, text FROM Messages', function(err, row) {
+//             io.emit('chat message', row.info);
+//             console.log('sending out old logs from' + select);
+//         });
+//         console.log('database: ' + select + ' selected');
+//     });
+//     socket.on('make', function(newDB) {
+//         console.log('got a message to create some life' + newDB);
+//         if (!exists)
+//             db.run("CREATE TABLE " + newDB + "(info TEXT)");
+//         console.log('doned it');
 
-    });
-});
+//     });
+// });
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
